bdabeb51d12f7d1e715618284b73aa441a1d4411
Patrick Reagan <reaganpr@gmail.com>
2013-06-26 22:31:07 -0600

    Rough implementation of login functionality

--- frameworks/backbone/js/app.js
+++ frameworks/backbone/js/app.js
@@ -9,17 +9,24 @@ app.User = Backbone.Model.extend({
   }
 });
 
+app.Session = Backbone.Model.extend({
+  url: '/session'
+})
+
 app.AppView = Backbone.View.extend({
   el: 'body',
 
   events: {
     'click #create-user': 'showNewUserForm',
-    'click #create-user.opened': 'closeNewUserForm'
+    'click #create-user.opened': 'closeNewUserForm',
+    'click #log-in': 'showLoginForm',
+    'click #log-in.opened': 'closeLoginForm'
   },
 
   initialize: function() {
-    this.$userFormToggle = $('#create-user');
-    this.$formDisplay    = $('#form-display');
+    this.$userFormToggle  = $('#create-user');
+    this.$loginFormToggle = $('#log-in');
+    this.$formDisplay     = $('#form-display');
   },
 
   showNewUserForm: function(event) {
@@ -32,11 +39,69 @@ app.AppView = Backbone.View.extend({
     this.$formDisplay.html(newUserForm.render().el);
   },
 
+  showLoginForm: function(event) {
+    event.preventDefault();
+    this.$loginFormToggle.addClass('opened');
+
+    var loginForm = new app.LoginForm();
+    this.listenTo(loginForm, 'close', this.closeLoginForm);
+    this.$formDisplay.html(loginForm.render().el);
+  },
+
   closeNewUserForm: function() {
     this.$formDisplay.html('');
     this.$userFormToggle.removeClass('opened');
+  },
+
+  closeLoginForm: function() {
+    this.$formDisplay.html('');
+    this.$loginFormToggle.removeClass('opened');
   }
 
+});
+
+app.LoginForm = Backbone.View.extend({
+  template: _.template($('#login-form').html()),
+
+  events: {
+    'submit form': 'create',
+    'click button[type=reset]': 'close'
+  },
+
+  create: function(event) {
+    event.preventDefault();
+
+    var session = new app.Session();
+    var formValues = this.$('form').serializeArray();
+
+    this.listenTo(session, 'sync', this.persistLogin);
+    this.listenTo(session, 'error', this.displayErrors);
+
+    _(formValues).each(function(pair) { session.set(pair.name, pair.value); });
+
+    session.save();
+  },
+
+  persistLogin: function(model, response) {
+    localStorage['userToken'] = response.token;
+    this.close();
+  },
+
+  displayErrors: function(model, xhr) {
+    _(xhr.responseJSON.errors).each(function(error) {
+      this.$('.errors').append('<li>' + error + '</li>');
+    });
+  },
+
+  close: function() {
+    this.stopListening();
+    this.trigger('close');
+  },
+
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  }
 });
 
 app.newUserForm = Backbone.View.extend({
